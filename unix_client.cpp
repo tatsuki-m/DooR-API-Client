4e91c4616eb9e70487b8241ae9113531b96bec70
tatsuki-m <tatsukitatsuki0923@gmail.com>
2016-11-18 02:46:35 +0900

    first commit

---
+++ unix_client.cpp
@@ -0,0 +1,85 @@
+#include "unix_client.h"
+
+const char* UnixClient::socket_name_ = "/tmp/unix-socket";
+
+UnixClient::UnixClient() {
+    ack_ = 1;
+    mem_key_ = 0;
+}
+
+UnixClient::~UnixClient() {
+}
+
+
+void
+UnixClient::run() {
+    create();
+    handle();
+    close_socket();
+}
+
+void
+UnixClient::close_socket() {
+    if (close(server_) == -1) {
+        perror("close");
+        exit(-1);
+    }
+}
+
+void
+UnixClient::create() {
+    struct sockaddr_un server_addr;
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sun_family = AF_UNIX;
+    strncpy(server_addr.sun_path, socket_name_, sizeof(server_addr.sun_path) - 1);
+
+    server_ = socket(PF_UNIX, SOCK_STREAM, 0);
+    if (!server_) {
+        perror("socket");
+        exit(-1);
+    }
+
+    if (connect(server_, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("connect");
+        exit(-1);
+    }
+}
+
+void
+UnixClient::handle() {
+    bool sent, success;
+    sent = send_ack();
+    if (not sent)
+        exit(-1);
+    success = get_response();
+    if (not success)
+        exit(-1);
+    printf("return value =%d", mem_key_);
+}
+
+
+bool
+UnixClient::send_ack() {
+    int ss;
+
+    if ((ss = send(server_, &ack_, sizeof(int), 0)) < 0) {
+        perror("write");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+bool
+UnixClient::get_response() {
+    int gs;
+
+    if ((gs = recv(server_, &mem_key_, sizeof(int), 0)) < 0 ) {
+        perror("read");
+        return false;
+    } else {
+        return true;
+    }
+}
+
