8f1f8bc72e037ce528019e7aacff325e7d1429eb
tatsuki-m <tatsukitatsuki0923@gmail.com>
2016-11-26 11:38:54 +0900

    generate shm key based num

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,29 @@
 #include "unix_client.h"
 #include "shared_memory.h"
 
+std::string getShmKey(int);
+
 int
 main() {
-    int shm_key;
+    int i_shmKey;
 
     // run socket communication
     UnixClient client = UnixClient();
-    shm_key = client.run();
+    i_shmKey = client.run();
 
-    std::cout << shm_key << std::endl;
+    std::string shmKey = getShmKey(i_shmKey);
+    std::cout << shmKey << std::endl;
+
+    SharedMemory *shm = new SharedMemory(shmKey);
+    std::cout << "generate worker" << std::endl;
+    delete shm;
+
     return 0;
-}
+};
+
+std::string getShmKey(int key) {
+    std::string shmKey = "shm_key" + std::to_string(key);
+    return shmKey;
+};
+
+
