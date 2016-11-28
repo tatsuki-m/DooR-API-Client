750860607e1e328ee2859ddae4f8bd1f737c2dd2
tatsuki-m <tatsukitatsuki0923@gmail.com>
2016-11-28 18:34:46 +0900

    define DoorApi class

--- src/door_api.cpp
+++ src/door_api.cpp
@@ -1,1 +1,14 @@
+#include "door_api.h"
+
+DoorApi::DoorApi() {
+    shm_ = new SharedMemory(shmKey);
+    shm_-> init();
+}
+
+DoorApi::~DoorApi() {
+    delete shm;
+}
+
+DoorApi::getShmKey() {
+}
 
