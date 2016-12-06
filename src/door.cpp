11efa2bbd0611f60f8f42a3a086ec82dbebb9f26
tatsuki-m <tatsukitatsuki0923@gmail.com>
2016-12-06 17:39:08 +0900

    define init funciton to shared memory

--- src/door.cpp
+++ src/door.cpp
@@ -1,8 +1,23 @@
 #include "door.h"
 
 Door::Door() {
+    baseShmKeyName_ = "shmKey0000";
+    // shared memory for baseShmKkey
+    init();
 }
 
 Door::~Door() {
+}
+
+bool
+Door::init() {
+    // init baseShmKey
+    SharedMemory* baseShmKey = new SharedMemory(baseShmKeyName_);
+    std::string bridgeShmKeyName = baseShmKey->initRead();
+    // init bridgeShmKey
+    SharedMemory* bridgeShmKey = new SharedMemory(bridgeShmKeyName);
+    std::string appShmKeyName_ = bridgeShmKey->initRead();
+    std::cout << appShmKeyName_ << std::endl;
+    return true;
 }
 
--- src/door.h
+++ src/door.h
@@ -1,15 +1,25 @@
 #ifndef DOOR_H_
 #define DOOR_H_
 
-#include <iostrem>
+#include <iostream>
 #include <string>
+
+#include "door_shared_memory.h"
+#include "shared_memory.h"
 
 class Door
 {
 public:
-    Door;
-    ~Door;
+    Door();
+    ~Door();
 private:
+    bool init();
 
-}:
+    std::string baseShmKeyName_;
+    std::string appShmKeyName_;
+
+    DoorSharedMemory* appShmKey_;
+};
+
 #endif
+
