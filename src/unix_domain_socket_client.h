d5c59f36b2d27d81524a82e36597b122d5d771e1
tatsuki-m <tatsukitatsuki0923@gmail.com>
2016-12-21 07:38:04 +0900

    change file name and refactoring

---
+++ src/unix_domain_socket_client.h
@@ -0,0 +1,45 @@
+#ifndef UNIX_DOMAIN_SOCKET_CLIENT_H_
+#define UNIX_DOMAIN_SOCKET_CLIENT_H_
+
+#pragma once
+
+#include <errno.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/un.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include "socket_ack.h"
+
+class UnixDomainSocketClient {
+
+public:
+    UnixDomainSocketClient(std::string, SocketType type);
+    ~UnixDomainSocketClient();
+
+    void run();
+    std::string getRecievedKey();
+
+private:
+    void create();
+    void handle();
+    bool sendAck();
+    bool getResponse();
+    void closeSocket();
+
+    struct SocketAck ack_;
+
+    int server_;
+    char shmKey_[16];
+    std::string socketName_;
+};
+
+#endif
+
