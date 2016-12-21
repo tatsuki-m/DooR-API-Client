f50d2931d4cb1d6185ee75b6fd8fc0bb26c25b38
tatsuki-m <tatsukitatsuki0923@gmail.com>
2016-12-22 02:35:22 +0900

    define step server, which connects library and server

---
+++ src/unix_domain_socket_step_server.h
@@ -0,0 +1,52 @@
+#ifndef UNIX_DOMAIN_SOCKET_STEP_SERVER_H_
+#define UNIX_DOMAIN_SOCKET_STEP_SERVER_H_
+
+#pragma once
+
+#include <errno.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/un.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "socket_ack.h"
+#include "unix_domain_socket_client.h"
+
+class UnixDomainSocketStepServer {
+
+public:
+    UnixDomainSocketStepServer(std::string);
+    ~UnixDomainSocketStepServer();
+
+    void run();
+    std::string getRecievedKey();
+
+private:
+    void serve();
+    void create();
+    void handle(int);
+    bool sendAck();
+    bool getRequest(int, SocketAck&);
+    void closeSocket();
+    void askShmKey(SocketAck&);
+    void sendDoorShmKey(int, SocketAck&);
+    std::string getSocketName();
+    std::string getShmKey();
+
+    int server_;
+    //TODO: move to bridge manager, in this class, just have pointer of it
+    std::vector<std::string> doorShmKeys_;
+    std::string librarySocketName_;
+    std::string workerSocketName_;
+};
+
+#endif
+
