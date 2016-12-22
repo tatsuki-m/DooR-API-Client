bcb3c5fe22c1c3792fdc56e6fdefaf618652d535
tatsuki-m <tatsukitatsuki0923@gmail.com>
2016-12-23 01:32:03 +0900

    move socket file to door brdge lib

---
+++ src/door_bridge/src/socket_ack.h
@@ -0,0 +1,15 @@
+#ifndef SOCKET_ACK_H_
+#define SOCKET_ACK_H_
+
+enum SocketType{
+    ASK_SOCKET = 0,
+    ASK_SHM = 1,
+};
+
+struct SocketAck {
+    SocketType type;
+    char data[64]={'\0'};
+};
+
+#endif
+
