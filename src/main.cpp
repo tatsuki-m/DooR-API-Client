2c90a17f5d983baf9ff10be6800f05ebd3e6b10d
tatsuki-m <tatsukitatsuki0923@gmail.com>
2017-01-08 18:27:01 +0900

    unlink step server socket

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 #include <string>
+#include <csignal>
 
 #include "bridge_manager.h"
 #include "door_unix_domain_socket_client/unix_domain_socket_client.h"
 #include "door_unix_domain_socket_client/socket_ack.h"
 
+std::string INIT_SOCKET_NAME = "/tmp/unix-socket/unix-socket";
+std::string STEP_SERVER_SOCKET_NAME = "/tmp/unix-socket/unix-socket-library";
+
+void
+signalHandler(int sigNum) {
+    std::cout << "Interrupt signal (" << sigNum << ") received." << std::endl;
+    unlink(STEP_SERVER_SOCKET_NAME.c_str());
+    std::cout << "Going to sleep.." <<std::endl;
+    exit(sigNum);
+}
+
 int
 main() {
-    // this shmKey for application to know the shared memory name, which connected client and server
+    signal(SIGINT, signalHandler);
 
     // run socket communication
-    std::string initSocketName = "/tmp/unix-socket/unix-socket";
     SocketType type = ASK_SOCKET;
-    UnixDomainSocketClient client = UnixDomainSocketClient(initSocketName, type);
+    UnixDomainSocketClient client = UnixDomainSocketClient(INIT_SOCKET_NAME, type);
     client.run();
     std::string workerSocketName = client.getRecievedData();
     std::cout << "main:: workerSocketName: " << workerSocketName << std::endl;
