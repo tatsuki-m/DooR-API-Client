22c1d8bec7803f78f1d9a29bfd53b50e9392c657
tatsuki-m <tatsukitatsuki0923@gmail.com>
2017-01-20 07:53:16 +0900

    change shared memory dir name

--- src/door_bridge/src/door_bridge.h
+++ src/door_bridge/src/door_bridge.h
@@ -7,10 +7,10 @@
 #include "door_unix_domain_socket_client/unix_domain_socket_client.h"
 #include "door_unix_domain_socket_client/socket_ack.h"
 #include "door_api/door_api.h"
-#include "door_shared_memory/shared_memory.h"
-#include "door_shared_memory/shared_packet_information.h"
-#include "door_shared_memory/dpi.h"
-#include "door_shared_memory/sync_semaphore.h"
+#include "door_ipc/shared_memory.h"
+#include "door_ipc/shared_packet_information.h"
+#include "door_ipc/dpi.h"
+#include "door_ipc/sync_semaphore.h"
 
 class DoorBridge
 {
